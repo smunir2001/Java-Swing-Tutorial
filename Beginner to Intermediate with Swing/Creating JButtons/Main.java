c6894feee8ba9e3d1d169498e257f1a0361128a8
Sami Munir <smunir2001@gmail.com>
2023-05-02 11:02:04 -0400

    adding tutorial Creating JButtons

---
+++ Beginner to Intermediate with Swing/Creating JButtons/Main.java
@@ -0,0 +1,9 @@
+/*
+ * JButton
+ * - a button that performs an action when clicked on.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }   
+}
