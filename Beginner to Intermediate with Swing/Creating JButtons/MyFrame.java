b6b87615c102c56359d3787cddd03e886d125246
Sami Munir <smunir2001@gmail.com>
2023-05-02 11:09:59 -0400

    adding button functionality

--- Beginner to Intermediate with Swing/Creating JButtons/MyFrame.java
+++ Beginner to Intermediate with Swing/Creating JButtons/MyFrame.java
@@ -1,11 +1,29 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
 
-public class MyFrame extends JFrame {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
 
     public MyFrame() {
+        button = new JButton();
+        button.setBounds(200, 100, 100, 50);
+        button.addActionListener(this);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
+        this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("poo");
+        }
     }
 }
