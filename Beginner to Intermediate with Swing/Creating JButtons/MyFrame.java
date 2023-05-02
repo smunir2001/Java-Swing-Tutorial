01e58c9d2bba793aaaeb0b5820fe5b230a970b21
Sami Munir <smunir2001@gmail.com>
2023-05-02 11:05:10 -0400

    adding MyFrame.java to Creating JButtons

---
+++ Beginner to Intermediate with Swing/Creating JButtons/MyFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500, 500);
+        this.setVisible(true);
+    }
+}
